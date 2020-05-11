a384ce05b117c96400528ad374f9019525394b13
Zizoubrown <zizumoha2015@gmail.com>
2020-05-11 07:41:45 +0300

    add jquery to Portfollio Section

--- JS/script.js
+++ JS/script.js
@@ -39,3 +39,54 @@ $(document).ready(function(){
     $("#hide2").click(function(){
     $("#hide2").hide();
     });
+
+     //Portfolio Section
+   $('#work8').hover(function(){
+    $('.overlay8').css("display","block")
+    },function(){
+        $('.overlay8').css("display","none")
+    })
+
+    $('#work7').hover(function(){
+        $('.overlay7').css("display","block")
+    },function(){
+        $('.overlay7').css("display","none")
+    })
+
+    $('#work6').hover(function(){
+        $('.overlay6').css("display","block")
+    },function(){
+        $('.overlay6').css("display","none")
+    })
+
+    $('#work5').hover(function(){
+        $('.overlay5').css("display","block")
+    },function(){
+        $('.overlay5').css("display","none")
+    })
+
+    $('#work4').hover(function(){
+        $('.overlay').css("display","block")
+    },function(){
+        $('.overlay').css("display","none")
+    })
+
+    $('#work3').hover(function(){
+        $('.overlay3').css("display","block")
+    },function(){
+        $('.overlay3').css("display","none")
+    })
+
+    $('#work2').hover(function(){
+        $('.overlay2').css("display","block")
+    },function(){
+        $('.overlay2').css("display","none")
+    })
+
+    $('#work1').hover(function(){
+        $('.overlay1').css("display","block")
+    },function(){
+        $('.overlay1').css("display","none")
+    })
+
+});
