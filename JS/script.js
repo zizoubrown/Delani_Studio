8b6bfbb5763b2dc81766b8e8c8b9a83798f99bc1
Zizoubrown <zizumoha2015@gmail.com>
2020-05-11 07:36:38 +0300

    add jquery to what we do section

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,41 @@
 $(document).ready(function(){
-    $('.icon').click(function(){
-        $('.hide').show();
+    //Design Section
+    $("#icon").click(function(){
+    $("#hide").show();
     });
-    $('.icon').click(function(){
-        $('.icon').hide();
+    $("#icon").click(function(){
+    $("#icon").hide();
     });
-    $('.hide')(function(){
-        $('.icon').show();
+    $("#hide").click(function(){
+    $("#icon").show();
     });
-    $('.hide')(function(){
-        $('.hide').hide();
+    $("#hide").click(function(){
+    $("#hide").hide();
     });
-});
+ 
+    //Development Section
+    $("#icon1").click(function(){
+    $("#hide1").show();
+    });
+    $("#icon1").click(function(){
+    $("#icon1").hide();
+    });
+    $("#hide1").click(function(){
+    $("#icon1").show();
+    });
+    $("#hide1").click(function(){
+    $("#hide1").hide();
+    });
+    //Product Section
+    $("#icon2").click(function(){
+    $("#hide2").show();
+    });
+    $("#icon2").click(function(){
+    $("#icon2").hide();
+    });
+    $("#hide2").click(function(){
+    $("#icon2").show();
+    });
+    $("#hide2").click(function(){
+    $("#hide2").hide();
+    });
